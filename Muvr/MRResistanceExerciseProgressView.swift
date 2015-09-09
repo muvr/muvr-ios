13910ceec2e4edffa06b46a896aff5bf3f89f407
Jan Machacek <jan.machacek@gmail.com>
2015-09-09 09:31:31 +0100

    UX patches

--- Muvr/MRResistanceExerciseProgressView.swift
+++ Muvr/MRResistanceExerciseProgressView.swift
@@ -7,24 +7,16 @@ class MRResistanceExerciseProgressView : UIView {
     @IBOutlet var bottomLabel: UILabel!
     @IBOutlet var time: MBCircularProgressBarView!
     @IBOutlet var repetitions: MBCircularProgressBarView!
-
-    #if (arch(i386) || arch(x86_64)) && os(iOS)
-    private let animationDuration = 2.0
-    #else
-    private let animationDuration = 0.5
-    #endif
     
+    private var expanded: Bool = false
     private var text: String! = ""
 
     private func updateLabel() -> Void {
         let secs = Int(time.value)
         let reps = Int(repetitions.value)
         let sb = NSMutableString()
-        if secs > 0 { sb.appendString("\(secs) s") }
-        if reps > 0 {
-            if sb.length > 0 { sb.appendString(" | ") }
-            sb.appendString("\(reps) r")
-        }
+        if secs > 0 { sb.appendString("\(secs) s\n") }
+        if reps > 0 { sb.appendString("\(reps) r") }
         topLabel.text = sb as String
     }
     
@@ -53,22 +45,28 @@ class MRResistanceExerciseProgressView : UIView {
     }
     
     func expand() {
+        if expanded { return }
+        expanded = true
+
         self.frame = self.superview!.frame
         self.layoutIfNeeded()
         
-        time.progressLineWidth = 30
-        repetitions.progressLineWidth = 30
-        topLabel.font = UIFont.systemFontOfSize(40, weight: 0.3)
+        time.progressLineWidth = frame.height / 25
+        repetitions.progressLineWidth = frame.height / 25
+        topLabel.font = UIFont.systemFontOfSize(frame.height / 10, weight: UIFontWeightUltraLight)
     }
     
     func collapse() {
-        let collapsed = CGRectMake(0, 0, frame.width, frame.width / 2)
+        if !expanded { return }
+        expanded = false
+
+        let collapsed = CGRectMake(0, 0, self.frame.width, self.frame.width / 2)
         self.frame = collapsed
         self.layoutIfNeeded()
         
-        topLabel.font = UIFont.systemFontOfSize(16, weight: 0.3)
-        time.progressLineWidth = 15
-        repetitions.progressLineWidth = 15
+        self.topLabel.font = UIFont.systemFontOfSize(self.frame.height / 10, weight: UIFontWeightUltraLight)
+        self.time.progressLineWidth = frame.height / 25
+        self.repetitions.progressLineWidth = frame.height / 25
     }
         
 }
