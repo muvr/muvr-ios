3b59e659cdc53bc7c28ab31ec79ae664839af1b9
Jan Machacek <jan.machacek@gmail.com>
2015-09-09 19:35:31 +0100

    Much improved session UI

---
+++ Muvr/MRResistanceExerciseSessionProgressView.swift
@@ -0,0 +1,26 @@
+import Foundation
+import MBCircularProgressBar
+
+class MRResistanceExerciseSessionProgressView : UIView {
+    @IBOutlet var view: UIView!
+    @IBOutlet var intensities: MRResistanceExerciseIntensityView!
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        NSBundle.mainBundle().loadNibNamed("MRResistanceExerciseSessionProgressView", owner: self, options: nil)
+        addSubview(view)
+    }
+    
+    func setResistenceExercises(exercises: [MRClassifiedResistanceExercise]) -> Void {
+        intensities.setResistenceExercises(exercises)
+    }
+
+    override var frame: CGRect {
+        didSet {
+            if view != nil {
+                view.frame = self.frame
+            }
+        }
+    }
+
+}
