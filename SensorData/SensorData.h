a351692d7ff2a79b1ca0737b936ac3a128def25b
Jan Machacek <jan.machacek@gmail.com>
2015-09-19 12:04:34 +0100

    Now with frameworks

---
+++ SensorData/SensorData.h
@@ -0,0 +1,19 @@
+//
+//  SensorData.h
+//  SensorData
+//
+//  Created by Jan Machacek on 19/09/2015.
+//  Copyright © 2015 Muvr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SensorData.
+FOUNDATION_EXPORT double SensorDataVersionNumber;
+
+//! Project version string for SensorData.
+FOUNDATION_EXPORT const unsigned char SensorDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SensorData/PublicHeader.h>
+
+
