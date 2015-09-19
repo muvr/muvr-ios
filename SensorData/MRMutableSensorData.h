7471a61cfa78dc1d37a369748985de3f3fec6934
Jan Machacek <jan.machacek@gmail.com>
2015-09-19 14:26:49 +0100

    cm

--- SensorData/MRMutableSensorData.h
+++ SensorData/MRMutableSensorData.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
+#import <CoreMotion/CoreMotion.h>
+#import <HealthKit/HealthKit.h>
 
 @interface MRMutableSensorData : NSObject
+
+- (void)append:(CMDeviceMotion *)data;
 
 @end
--- SensorDataTests/SensorDataTests.swift
+++ SensorDataTests/SensorDataTests.swift
@@ -13,6 +13,8 @@ class SensorDataTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        let x = MRMutableSensorData()
+    
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
