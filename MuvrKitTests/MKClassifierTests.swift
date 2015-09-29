c2d2db8bb2a43e309c28ea4d2336679022990f0f
Jan Machacek <jan.machacek@gmail.com>
2015-09-29 21:04:26 +0200

    Classification handling

--- MuvrKitTests/MKClassifierTests.swift
+++ MuvrKitTests/MKClassifierTests.swift
@@ -4,8 +4,8 @@ import XCTest
 
 class MKClassifierTests : XCTestCase {
     lazy var classifier: MKClassifier = {
-        let data = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("test-model", ofType: "raw")!)!
-        let model = MKExerciseModel(layerConfig: [], weights: data,
+        let data = NSData(contentsOfFile: NSBundle(forClass: MKClassifierTests.self).pathForResource("test-model", ofType: "raw")!)!
+        let model = MKExerciseModel(layerConfig: [1200, 250, 100, 3], weights: data,
             sensorDataTypes: [.Accelerometer(location: .LeftWrist)],
             exerciseIds: ["a", "b", "c"])
         return MKClassifier(model: model)
@@ -14,7 +14,7 @@ class MKClassifierTests : XCTestCase {
     /// 
     /// Tests that the classification rejects insufficient data
     ///
-    func testClassifyNotEnoughData() {
+    func testClassifyNoSensorDataType() {
         do {
             try classifier.classify(block: MKSensorData(types: [.HeartRate], start: 0, samplesPerSecond: 1, samples: []), maxResults: 100)
             XCTFail("No exception")
@@ -23,5 +23,20 @@ class MKClassifierTests : XCTestCase {
             XCTFail("Bad exception")
         }
     }
+    
+    ///
+    /// Tests that the classification rejects insufficient data
+    ///
+    func testClassifyNotEnoughData() {
+        do {
+            try classifier.classify(block: MKSensorData(types: [.Accelerometer(location: .LeftWrist)], start: 0, samplesPerSecond: 1, samples: [1,2,3]), maxResults: 100)
+            XCTFail("No exception")
+        } catch MKClassifierError.NotEnoughRows(_) {
+        } catch {
+            XCTFail("Bad exception")
+        }
+    }
+    
+    
     
 }
