f7bea6a866f0e7757711a709ad72aaab3fdef76d
Jan Machacek <jan.machacek@gmail.com>
2015-09-29 10:58:43 +0200

    estimator structure

--- MuvrKit/MKRepetitionEstimator.swift
+++ MuvrKit/MKRepetitionEstimator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Accelerate
 
 public enum MKRepetitionEstimatorError : ErrorType {
     
@@ -9,33 +10,34 @@ public enum MKRepetitionEstimatorError : ErrorType {
 public struct MKRepetitionEstimator {
     public typealias Estimate = (UInt8, Double)
     
+    
+//    private func autocorrelation(data: [Float], dimension: Int) -> [Float] {
+//        let filterLength = data.count
+//        let resultLength = data.count
+//        var correlation = [Float](count: resultLength, repeatedValue: 0)
+//        var signal = data + [Float](count: filterLength + 1, repeatedValue: 0)
+//        
+//        (0..<dimension).forEach { d in
+//            let dataPtr = UnsafePointer<Float>(data) + d
+//            let correlationPtr: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>(from: &correlation)
+//            
+//            vDSP_conv(&signal + d, vDSP_Stride(dimension), dataPtr, vDSP_Stride(dimension), correlationPtr.advancedBy(d), vDSP_Stride(dimension), vDSP_Length(resultLength), vDSP_Length(filterLength))
+//        }
+//    }
+// 
+//    
+//    private func numberOfRepetitions(data: [Float], dimension: Int) -> Estimate {
+//        
+//    }
+//
+
     public func estimate(data data: MKSensorData) throws -> Estimate {
         let (d, v) = data.samples(along: [.Accelerometer(location: .LeftWrist), .Accelerometer(location: .RightWrist)])
         if d == 0 { throw MKRepetitionEstimatorError.MissingAccelerometerType }
+        
+        
 
         fatalError()
     }
     
 }
-
-/*
-@interface MRRepetitionEstimator : NSObject
-
-// A characteristic profile of a periode of a signal
-struct PeriodicProfile
-{
-// Abosolute amount the signal changed in the period
-uint total_steps = 0;
-// Upwards steps of the signal
-uint upward_steps = 0;
-// Downwards steps of the signal
-uint downward_steps = 0;
-};
-
-//
-// Estimate the number of exercise repetitions in the passed data
-//
-- (uint)estimate:(const std::vector<muvr::fused_sensor_data>&)data;
-
-@end
-*/
