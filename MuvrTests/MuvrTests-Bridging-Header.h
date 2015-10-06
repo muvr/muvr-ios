b7b4b6756c69b8209ceb417db52f3baf31a25a9c
Jan Machacek <jan.machacek@gmail.com>
2015-10-06 20:08:06 +0100

    Tidy up

--- MuvrTests/MuvrTests-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#ifndef Muvr_MuvrTests_Bridging_Header_h
-#define Muvr_MuvrTests_Bridging_Header_h
-
-#import <CommonCrypto/CommonCrypto.h>
-#import "../Muvr/MRClassifiedResistanceExercise.h"
-#import "../Muvr/MRModelParameters.h"
-
-#endif
