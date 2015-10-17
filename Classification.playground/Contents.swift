b640733a997ba34c3439d0841159c46c6f139f14
Jan Machacek <jan.machacek@gmail.com>
2015-10-17 21:09:09 +0100

    Playground

--- Classification.playground/Contents.swift
+++ Classification.playground/Contents.swift
@@ -1,7 +1,9 @@
 //: # Whole-session classification
 //: The core idea is to slide window over the input and watch the variation
-//: of the classified data
-
+//: of the classified data.
+//: Our hypothesis is that there is some kind of probabilistic model that 
+//: can identify exercise blocks given the classification result on each
+//: window.
 import UIKit
 import XCPlayground
 @testable import MuvrKit
@@ -12,7 +14,7 @@ func model(named name: String) -> MKExerciseModel {
     let weightsData = NSData(contentsOfFile: demoModelPath)!
     let model = MKExerciseModel(layerConfig: [1200, 250, 100, 3], weights: weightsData,
         sensorDataTypes: [.Accelerometer(location: .LeftWrist)],
-        exerciseIds: ["1", "2", "3"])
+        exerciseIds: ["biceps-curl", "triceps-extension", "lateral-raise"])
     return model
 }
 
@@ -27,18 +29,17 @@ let sd = try! MKSensorData(decoding: NSData(contentsOfFile: mostlyExerciseData)!
 
 //: ### Now run the sliding windows
 let windowSize = 50
-(0..<100).forEach { wi in
+(0..<900).forEach { wi in
     let windowSamples = sd.samples(along: [.Accelerometer(location: .LeftWrist)], range: Range<Int>(start: windowSize * wi, end: windowSize * wi + 400)).1
 
     // display window data
-    windowSamples.enumerate().forEach { i, x in if i % 3 == 0 { x } }
+    // windowSamples.enumerate().forEach { i, x in if i % 3 == 0 { x } }
 
     // compute the window
     let window = try! MKSensorData(types: sd.types, start: sd.start, samplesPerSecond: sd.samplesPerSecond, samples: windowSamples)
     
     // classify
     let windowClassification = try! classifier.classify(block: window, maxResults: 10)
-    print("-------------")
-    windowClassification.forEach { x in print(x) }
+    windowClassification.first.map { x in print(x) }
 }
 
