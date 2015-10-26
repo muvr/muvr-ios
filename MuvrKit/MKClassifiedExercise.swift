d30520d8d035fe61b061565cbb486ad33f44c62b
samudurand <samueld@cakesolutions.net>
2015-10-26 16:41:10 +0000

    cleanup

--- MuvrKit/MKClassifiedExercise.swift
+++ MuvrKit/MKClassifiedExercise.swift
@@ -10,13 +10,4 @@ public struct MKClassifiedExercise {
     public let repetitions: UInt?
     public let intensity: MKExerciseIntensity?
     public let weight: Double?
-    
-    public init(confidence: Double, exerciseId: MKExerciseId, duration: NSTimeInterval, repetitions: UInt?, intensity: MKExerciseIntensity?, weight: Double?) {
-        self.confidence = confidence
-        self.exerciseId = exerciseId
-        self.duration = duration
-        self.repetitions = repetitions
-        self.intensity = intensity
-        self.weight = weight
-    }
 }
