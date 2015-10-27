860c295c32b9b25c6749aa7f5e06b9cff907530f
Jan Machacek <jan.machacek@gmail.com>
2015-10-27 20:53:28 +0000

    Demo mode table

--- Watch Extension/MRMainController.swift
+++ Watch Extension/MRMainController.swift
@@ -3,14 +3,29 @@ import Foundation
 import WatchConnectivity
 import MuvrKit
 
+class MRExerciseRow: NSObject {
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+    
+    func setExercise(exercise: (String, String)) {
+        textLabel.setText(exercise.1)
+    }
+}
+
 class MRMainController: WKInterfaceController, MRSessionProgressGroup {
     @IBOutlet weak var exerciseModel: WKInterfacePicker!
     @IBOutlet weak var startGroup: WKInterfaceGroup!
     @IBOutlet weak var progressGroup: WKInterfaceGroup!
+    @IBOutlet weak var exercisesTable: WKInterfaceTable!
     
     @IBOutlet weak var titleLabel: WKInterfaceLabel!
     @IBOutlet weak var timeLabel: WKInterfaceLabel!
     @IBOutlet weak var statsLabel: WKInterfaceLabel!
+    
+    private let exercises = [
+        ("arms/biceps-curl", "Biceps curl"),
+        ("arms/triceps-extension", "Triceps extension"),
+        ("arms/lateral-raise", "Lateral raise")
+    ]
 
     private var renderer: MRSessionProgressGroupRenderer?
 
@@ -37,10 +52,22 @@ class MRMainController: WKInterfaceController, MRSessionProgressGroup {
         if sd.currentSession != nil {
             addMenuItemWithItemIcon(WKMenuItemIcon.Pause, title: "Pause", action: "pause")
             addMenuItemWithItemIcon(WKMenuItemIcon.Trash, title: "Stop",  action: "stop")
+
+            exercisesTable.setNumberOfRows(exercises.count, withRowType: "exercise")
+            (0..<exercisesTable.numberOfRows).forEach { i in
+                let row = exercisesTable.rowControllerAtIndex(i) as! MRExerciseRow
+                row.setExercise(exercises[i])
+            }
+        } else {
+            exercisesTable.setNumberOfRows(0, withRowType: "exercise")
         }
         
         progressGroup.setHidden(sd.currentSession == nil)
         startGroup.setHidden(sd.currentSession != nil)
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        NSLog("Selected \(rowIndex)")
     }
     
     func pause() {
@@ -60,6 +87,9 @@ class MRMainController: WKInterfaceController, MRSessionProgressGroup {
         updateUI()
     }
     
+    ///
+    /// Called when the user clicks the start demo button
+    ///
     @IBAction func beginDemo() {
         MRExtensionDelegate.sharedDelegate().startSession(exerciseModelMetadataIndex: exerciseModelMetadataIndex, demo: true)
         updateUI()
