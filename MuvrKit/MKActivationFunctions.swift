7695fd8b40b84c8970e8ff4357822c4ac6139e37
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-10-29 19:44:56 +0100

    improved vdsp call style

--- MuvrKit/MKActivationFunctions.swift
+++ MuvrKit/MKActivationFunctions.swift
@@ -6,45 +6,34 @@ import Accelerate
 ///
 public func sigmoidActivation(inout input: [Float], offset: Int, length: Int) {
     var one: Float = 1.0
-    var mone: Float = -1.0
+    var minusOne: Float = -1.0
+    let inputPointer: UnsafeMutablePointer<Float> = UnsafeMutablePointer(input).advancedBy(offset)
     
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vDSP_vneg(source, vDSP_Stride(1), &input[offset], vDSP_Stride(1), vDSP_Length(length))
-    }
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vvexpf(&input[offset], source, [Int32(length)])
-    }
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vDSP_vsadd(source, vDSP_Stride(1), &one, &input[offset], vDSP_Stride(1), vDSP_Length(length))
-    }
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vvpowsf(&input[offset], &mone, source, [Int32(length)])
-    }
+    vDSP_vneg(inputPointer, vDSP_Stride(1), inputPointer, vDSP_Stride(1), vDSP_Length(length))
+    
+    vvexpf(inputPointer, inputPointer, [Int32(length)])
+    
+    vDSP_vsadd(inputPointer, vDSP_Stride(1), &one, inputPointer, vDSP_Stride(1), vDSP_Length(length))
+    
+    vvpowsf(inputPointer, &minusOne, inputPointer, [Int32(length)])
 }
 
 ///
 /// Use the tangens as an activation
 ///
 public func tangentActivation(inout input: [Float], offset: Int, length: Int) {
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vvtanhf(&input[offset], source, [Int32(length)])
-    }
+    let inputPointer: UnsafeMutablePointer<Float> = UnsafeMutablePointer(input).advancedBy(offset)
+
+    vvtanhf(inputPointer, inputPointer, [Int32(length)])
 }
 
 ///
 /// Rectified Linear Unit, it computes point-wise y=max(0,x)
 ///
 public func reLUActivation(inout input: [Float], offset: Int, length: Int) {
-    let threshold: Float = 0.0
-    input.withUnsafeBufferPointer{ sourcePointer  in
-        let source = sourcePointer.baseAddress.advancedBy(offset)
-        vDSP_vthres(source, vDSP_Stride(1), [threshold], &input[offset], vDSP_Stride(1), vDSP_Length(length))
-    }
+    var threshold: Float = 0.0
+
+    vDSP_vthres(&input + offset, vDSP_Stride(1), &threshold, &input + offset, vDSP_Stride(1), vDSP_Length(length))
 }
 
 ///
