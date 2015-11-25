ed758c147e77ec770acdb8c76a5bcfb52e9f318e
Duc Hoang <duch@cakesolutions.net>
2015-11-25 10:58:49 +0000

    Update code in playground

--- Classification.playground/Contents.swift
+++ Classification.playground/Contents.swift
@@ -64,21 +64,13 @@ func model(named name: String, layerConfiguration: String, labels: [String]) thr
     return model
 }
 
+func getModel(id id: String) throws -> MKExerciseModel {
+    return try MKExerciseModel(fromBundle: NSBundle.mainBundle(), id: id)
+}
 //: ### Construct a classifier
-let exerciseClassifier = try! MKClassifier(model: model(named: "arms_model.weights",
-    layerConfiguration: "1200 id 250 relu 50 relu 3 logistic",
-    labels: ["shoulders/lateral-raise", "arms/biceps-curl", "arms/triceps-extension"]))
+let exerciseClassifier = try! MKClassifier(model: getModel(id: "arms"))
 
-
-let slackingClassifier = try! MKClassifier(model: model(named: "Nov12_slacking_model.weights",
-    layerConfiguration: "1200 id 500 relu 100 relu 25 relu 2 logistic",
-    labels: ["none", "exercise"]))
-
-let eneClassifier = try! MKClassifier(model: model(named: "slacking_model.weights",
-    layerConfiguration: "1200 id 250 relu 50 relu 2 logistic",
-    labels: ["-", "E"]))
-
-
+let eneClassifier = try! MKClassifier(model: getModel(id: "slacking"))
 //: ### Load the data from the session
 //let resourceName = "no-movement-face-up"
 //let resourceName = "bc-only"
@@ -104,9 +96,9 @@ let windowSize = 400
 
 // test slacking dataset
 print("EVALUATE SLACKING MODEL\n")
-//let slk = try! slackingClassifier.classify(block: slackingData.sliceByCSVRow(from: 3303, to: 4440), maxResults: 2) // EXERCISE
-//let slk = try! slackingClassifier.classify(block: slackingData.sliceByCSVRow(from: 9097, to: 9980), maxResults: 2) // EXERCISE
-let slk = try! slackingClassifier.classify(block: slackingData.sliceByCSVRow(from: 12401, to: 13252), maxResults: 2) // EXERCISE
+//let slk = try! eneClassifier.classify(block: slackingData.sliceByCSVRow(from: 3303, to: 4440), maxResults: 2) // EXERCISE
+//let slk = try! eneClassifier.classify(block: slackingData.sliceByCSVRow(from: 9097, to: 9980), maxResults: 2) // EXERCISE
+let slk = try! eneClassifier.classify(block: slackingData.sliceByCSVRow(from: 12401, to: 13252), maxResults: 2) // EXERCISE
 print("\n\nRESULT:")
 slk.forEach { wcls in print(wcls) }
 print("END\n")
