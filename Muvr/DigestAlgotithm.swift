a6b55bb37c4d55fbd3070367b8409e14b719e3e6
Damien Bailly <damien.bailly@gmail.com>
2015-12-09 21:38:11 +0000

    Remove unnecessary digest algorithms

--- Muvr/DigestAlgotithm.swift
+++ Muvr/DigestAlgotithm.swift
@@ -3,17 +3,13 @@ import Foundation
 typealias HmacDigest = [Int8]
 
 enum DigestAlgorithm {
-    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    case MD5, SHA256
     
     var hmacAlgorithm: CCHmacAlgorithm {
         var result: Int = 0
         switch self {
         case .MD5:      result = kCCHmacAlgMD5
-        case .SHA1:     result = kCCHmacAlgSHA1
-        case .SHA224:   result = kCCHmacAlgSHA224
         case .SHA256:   result = kCCHmacAlgSHA256
-        case .SHA384:   result = kCCHmacAlgSHA384
-        case .SHA512:   result = kCCHmacAlgSHA512
         }
         return CCHmacAlgorithm(result)
     }
@@ -22,11 +18,7 @@ enum DigestAlgorithm {
         var result: Int32 = 0
         switch self {
         case .MD5:      result = CC_MD5_DIGEST_LENGTH
-        case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
-        case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
         case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
-        case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
-        case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
         }
         return Int(result)
     }
@@ -37,11 +29,7 @@ enum DigestAlgorithm {
         
         switch self {
         case .MD5:      CC_MD5(data, UInt32(dataLen), buffer)
-        case .SHA1:     CC_SHA1(data, UInt32(dataLen), buffer)
-        case .SHA224:   CC_SHA224(data, UInt32(dataLen), buffer)
         case .SHA256:   CC_SHA256(data, UInt32(dataLen), buffer)
-        case .SHA384:   CC_SHA384(data, UInt32(dataLen), buffer)
-        case .SHA512:   CC_SHA512(data, UInt32(dataLen), buffer)
         }
         
         let hash = toBytes(buffer)
