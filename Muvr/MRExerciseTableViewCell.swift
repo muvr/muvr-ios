bb6564c6e8687b0f99e1229128f4d9d1731fa8db
Jan Machacek <jan.machacek@gmail.com>
2015-12-31 18:13:43 +0000

    Better views

--- Muvr/MRExerciseTableViewCell.swift
+++ Muvr/MRExerciseTableViewCell.swift
@@ -2,13 +2,26 @@ import UIKit
 import MuvrKit
 
 class MRExerciseTableViewCell : UITableViewCell {
+    static let nib: UINib = UINib(nibName: "MRExerciseTableViewCell", bundle: nil)
+    static let cellReuseIdentifier: String = "ex"
+
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var repetitionsLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     
     func setClassifiedExercise(classifiedExercise: MKClassifiedExercise, lastClassifiedExercise: MKClassifiedExercise?) {
-        
+        exerciseLabel.text = classifiedExercise.exerciseId
+        repetitionsLabel.text = classifiedExercise.repetitions.map { String($0) } ?? ""
+        intensityLabel.text = classifiedExercise.intensity.map { String($0) } ?? ""
+        weightLabel.text = classifiedExercise.weight.map { "\($0) kg" } ?? ""
     }
     
     func setPlannedExecise(plannedExercise: MKPlannedExercise, lastPlannedExercise: MKPlannedExercise?) {
-        
+        exerciseLabel.text = plannedExercise.exerciseId
+        repetitionsLabel.text = plannedExercise.repetitions.map { String($0) } ?? ""
+        intensityLabel.text = plannedExercise.intensity.map { String($0) } ?? ""
+        weightLabel.text = plannedExercise.weight.map { "\($0) kg" } ?? ""
     }
     
 }
--- Muvr/MRExercisingViewController.swift
+++ Muvr/MRExercisingViewController.swift
@@ -1,15 +1,17 @@
 import UIKit
 
-class MRExercisingViewController : UIViewController, UITableViewDataSource {
+class MRExercisingViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var timedView: MRTimedView!
 
     override func viewDidLoad() {
         timedView.setColourScheme(MRColourSchemes.amber)
         timedView.elapsedResets = true
+        tableView.registerNib(MRExerciseTableViewCell.nib, forCellReuseIdentifier: MRExerciseTableViewCell.cellReuseIdentifier)
     }
     
     override func viewDidAppear(animated: Bool) {
+        tableView.allowsSelection = false
         timedView.start(5, onTimerElapsed: beginExercising)
     }
     
@@ -22,14 +24,49 @@ class MRExercisingViewController : UIViewController, UITableViewDataSource {
     private func stopExercising(tv: MRTimedView) {
         timedView.setColourScheme(MRColourSchemes.green)
         timedView.setButtonTitle("✓")
+        tableView.allowsSelection = true
+        tableView.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        switch section {
+        case 0: return 0
+        case 1: return 1
+        case 2: return 1
+        default: fatalError("Match error")
+        }
+    }
+    
+    func tableView(tableView: UITableView, accessoryTypeForRowWithIndexPath indexPath: NSIndexPath) -> UITableViewCellAccessoryType {
+        if tableView.allowsSelection {
+            return UITableViewCellAccessoryType.DisclosureIndicator
+        } else {
+            return UITableViewCellAccessoryType.None
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        fatalError()
+        switch indexPath.section {
+        case 0: fatalError()
+        case 1:
+            let cell = tableView.dequeueReusableCellWithIdentifier("other", forIndexPath: indexPath) 
+            cell.textLabel?.text = "Something else"
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCellWithIdentifier("other", forIndexPath: indexPath)
+            cell.textLabel?.text = "Nothing"
+            return cell
+        default: fatalError("Match error")
+        }
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // TODO: perform labelling
+        navigationController?.popViewControllerAnimated(true)
     }
     
 }
