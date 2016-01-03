df27e3d4f4306647170d0bd95bf0168ee0abdb64
Jan Machacek <jan.machacek@gmail.com>
2016-01-03 22:04:09 +0000

    Location

--- MuvrKit/MKPhoneSensorDataSplitter.swift
+++ MuvrKit/MKPhoneSensorDataSplitter.swift
@@ -100,35 +100,6 @@ public class MKSensorDataSplitter {
         eneClassifier = try! MKClassifier(model: slackingModel)
     }
     
-    /*
-private func classify(exerciseModelId exerciseModelId: MKExerciseModelId, sensorData: MKSensorData) -> [MKClassifiedExercise]? {
-do {
-let exerciseModel = try exerciseModelSource.getExerciseModel(id: exerciseModelId)
-let exerciseClassifier = try MKClassifier(model: exerciseModel)
-
-let results = try eneClassifier.classify(block: sensorData, maxResults: 2)
-NSLog("Exercise / no exercise \(results)")
-return try results.flatMap { result -> [MKClassifiedExercise] in
-if result.exerciseId == "E" && result.duration >= exerciseModel.minimumDuration {
-// this is an exercise block - get the corresponding data section
-let data = try sensorData.slice(result.offset, duration: result.duration)
-// classify the exercises in this block
-let exercises = try! exerciseClassifier.classify(block: data, maxResults: 10)
-// adjust the offset with the offset from the original block
-// the offset returned by the classifier is relative to the current exercise block
-let (repetitions, _) = try self.repetitionEstimator.estimate(data: data)
-return exercises.map(self.shiftOffset(result.offset)).map(self.updateRepetitions(repetitions))
-} else {
-return []
-}
-}
-} catch let ex {
-NSLog("Failed to classify block: \(ex)")
-return []
-}
-}
-*/
-
     private func hintedSplit(from: NSTimeInterval, data: MKSensorData, hints: [MKClassificationHint]) -> Split {
         // first, filter hints that appear after ``from``
         let applicableHints = hints.filter { $0.start >= from }
@@ -145,6 +116,35 @@ return []
     }
 
     private func automatedSplit(from: NSTimeInterval, data: MKSensorData) -> Split {
+        // TODO: Reimplement me
+        /*
+        private func classify(exerciseModelId exerciseModelId: MKExerciseModelId, sensorData: MKSensorData) -> [MKClassifiedExercise]? {
+        do {
+        let exerciseModel = try exerciseModelSource.getExerciseModel(id: exerciseModelId)
+        let exerciseClassifier = try MKClassifier(model: exerciseModel)
+        
+        let results = try eneClassifier.classify(block: sensorData, maxResults: 2)
+        NSLog("Exercise / no exercise \(results)")
+        return try results.flatMap { result -> [MKClassifiedExercise] in
+        if result.exerciseId == "E" && result.duration >= exerciseModel.minimumDuration {
+        // this is an exercise block - get the corresponding data section
+        let data = try sensorData.slice(result.offset, duration: result.duration)
+        // classify the exercises in this block
+        let exercises = try! exerciseClassifier.classify(block: data, maxResults: 10)
+        // adjust the offset with the offset from the original block
+        // the offset returned by the classifier is relative to the current exercise block
+        let (repetitions, _) = try self.repetitionEstimator.estimate(data: data)
+        return exercises.map(self.shiftOffset(result.offset)).map(self.updateRepetitions(repetitions))
+        } else {
+        return []
+        }
+        }
+        } catch let ex {
+        NSLog("Failed to classify block: \(ex)")
+        return []
+        }
+        }
+        */
         return ([], [], from + data.duration)
     }
 
