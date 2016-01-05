42278c7cbe87ff7c0cdea07a11a8faca0836ffee
Jan Machacek <jan.machacek@gmail.com>
2016-01-05 22:15:27 +0000

    Regressions: linear and test failures.

--- Muvr/Entities/MRManagedClassifiedExercise+CoreDataAnalytics.swift
+++ Muvr/Entities/MRManagedClassifiedExercise+CoreDataAnalytics.swift
@@ -2,6 +2,10 @@ import Foundation
 import MuvrKit
 import CoreData
 
+///
+/// Adds basic (i.e. single-user) statistics for the classified exercises.
+/// This is typically used on the home page.
+///
 extension MRManagedClassifiedExercise {
     
     ///
@@ -10,18 +14,34 @@ extension MRManagedClassifiedExercise {
     /// repetitions, weight and duration.
     ///
     struct Average {
+        /// The exercise id (or part of the exercise id)
         let exerciseId: MKExerciseId
+        /// The number of entries that make up the average
         let count: Int
 
+        /// The intensity
         let averageIntensity: Double
+        /// The repetitions
         let averageRepetitions: Int
+        /// The weight
         let averageWeight: Double
+        /// The duration
         let averageDuration: NSTimeInterval
         
+        ///
+        /// A zero value suited for ``Array.reduce``.
+        /// - parameter exerciseId: the exercise id
+        /// - returns: the 0 element
+        ///
         static func zero(exerciseId: MKExerciseId) -> Average {
             return Average(exerciseId: exerciseId, count: 0, averageIntensity: 0, averageRepetitions: 0, averageWeight: 0, averageDuration: 0)
         }
         
+        ///
+        /// Adds ``that`` to this value
+        /// - parameter that: the other value
+        /// - returns: self + that
+        ///
         private func plus(that: Average) -> Average {
             assert(that.exerciseId == self.exerciseId)
             
@@ -34,17 +54,26 @@ extension MRManagedClassifiedExercise {
             )
         }
         
-        private func div(by: Double) -> Average {
+        ///
+        /// Divides ``self`` by ``const``
+        /// - parameter const: the constant to divide the values by
+        /// - returns: the updated value
+        ///
+        private func divideBy(const: Double) -> Average {
             return Average(exerciseId: exerciseId,
                 count: count,
-                averageIntensity: averageIntensity / by,
-                averageRepetitions: Int(Double(averageRepetitions) / by),
-                averageWeight: averageWeight / by,
-                averageDuration: averageDuration / by
+                averageIntensity: averageIntensity / const,
+                averageRepetitions: Int(Double(averageRepetitions) / const),
+                averageWeight: averageWeight / const,
+                averageDuration: averageDuration / const
             )
         }
     }
     
+    ///
+    /// Expression description for ``COUNT(_objectId)``, where ``_objectId`` is the identifier
+    /// of whatever entity it is being applied to.
+    ///
     private static let countByEntity: NSExpressionDescription = {
         let count = NSExpressionDescription()
         count.name = "count"
@@ -53,6 +82,11 @@ extension MRManagedClassifiedExercise {
         return count
     }()
     
+    ///
+    /// Expression description for ``average:`` of ``keyPath``.
+    /// - parameter keyPath: the property's KP
+    /// - returns: the expression description
+    ///
     private static func averageFor(keyPath: String) -> NSExpressionDescription {
         let expr = NSExpressionDescription()
         expr.name = "\(keyPath)"
@@ -61,25 +95,46 @@ extension MRManagedClassifiedExercise {
         return expr
     }
     
+    ///
+    /// TODO: this is a bit odd. It would be best to define MKExerciseId a full
+    /// TODO: type, not just typealias, and then add this to the new type.
+    ///
+    /// Returns a component of the exerciseId at the given index
+    /// - parameter index: the index 0..(number of slashes)
+    /// - parameter exerciseId: the exercise id
+    /// - returns: the element at the given index
+    ///
     private static func componentAt(index: Int, exerciseId: String) -> String? {
         let components = exerciseId.componentsSeparatedByString("/")
         if index >= 0 && index < components.count { return components[index] }
         return nil
     }
 
+    ///
+    /// Computes the averages for the ``MRManagedClassifiedExercise`` in the given ``managedObjectContext``,
+    /// whose ``exerciseId`` starts with ``exerciseIdPrefix``, dropping the ``exerciseIdPrefix`` from the
+    /// exerciseIds returned.
+    /// - parameter managedObjectContext: the MOC
+    /// - parameter exerciseIdPrefix: the exercise id prefix to match (i.e. back/, arms/)
+    /// - returns: the average for the last 100 sessions
+    ///
     static func averages(inManagedObjectContext managedObjectContext: NSManagedObjectContext, exerciseIdPrefix: String?) -> [Average] {
+        let fetchLimit = 100
         let entity = NSEntityDescription.entityForName("MRManagedClassifiedExercise", inManagedObjectContext: managedObjectContext)
         let exerciseId = entity!.propertiesByName["exerciseId"]!
 
         let fetchRequest = NSFetchRequest(entityName: "MRManagedClassifiedExercise")
         fetchRequest.propertiesToFetch = [exerciseId, countByEntity, averageFor("duration"), averageFor("cdWeight"), averageFor("cdIntensity"), averageFor("cdRepetitions")]
         fetchRequest.propertiesToGroupBy = [exerciseId]
-        if let exerciseIdPrefix = exerciseIdPrefix {
+        if var exerciseIdPrefix = exerciseIdPrefix where !exerciseIdPrefix.isEmpty {
+            if exerciseIdPrefix.characters.last! != "/" {
+                exerciseIdPrefix = exerciseIdPrefix + "/"
+            }
             fetchRequest.predicate = NSPredicate(format: "exerciseId LIKE %@", exerciseIdPrefix + "*")
         }
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "exerciseSession.start", ascending: false)]
         fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
-        fetchRequest.fetchLimit = 100
+        fetchRequest.fetchLimit = fetchLimit
         
         // exerciseIdComponentIndex is the number of "/" in ``exerciseIdPrefix`` if given or 0
         let exerciseIdComponentIndex = exerciseIdPrefix.map { $0.characters.reduce(0) { r, c in if c == "/" { return r + 1 } else { return r } } } ?? 0
@@ -107,32 +162,11 @@ extension MRManagedClassifiedExercise {
             return averages.values.map { averages in
                 let z = Average.zero(averages.first!.exerciseId)
                 let reduced = averages.reduce(z) { $0.plus($1) }
-                return reduced.div(Double(averages.count))
+                return reduced.divideBy(Double(averages.count))
             }
         }
         
         return []
     }
-//    
-//    static func summary(inManagedObjectContext managedObjectContext: NSManagedObjectContext) {
-//        // volume: sum(duration)
-//        // weight: sum(weight)
-//        // intensity: sum(intensity)
-//        // fitness: (weight * volume * intensity) / count
-//        
-//        let entity = NSEntityDescription.entityForName("MRManagedClassifiedExercise", inManagedObjectContext: managedObjectContext)
-//        let exerciseSession = entity!.propertiesByName["exerciseSession"]!
-//        
-//        let fetchRequest = NSFetchRequest(entityName: "MRManagedClassifiedExercise")
-//        fetchRequest.propertiesToFetch = [countByEntity, sumFor("duration"), sumFor("cdWeight"), sumFor("cdIntensity"), sumFor("cdRepetitions")]
-//        fetchRequest.propertiesToGroupBy = [exerciseSession]
-//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "exerciseSession.start", ascending: false)]
-//        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
-//        fetchRequest.fetchLimit = 100
-//        
-//        let result = try? managedObjectContext.executeFetchRequest(fetchRequest)
-//        NSLog("\(result)")
-//    }
-    
 
 }
