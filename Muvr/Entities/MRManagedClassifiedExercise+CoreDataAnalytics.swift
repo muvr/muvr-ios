8eab218024dd9487fa3bffd740363a0bb5f9c2ca
Jan Machacek <jan.machacek@gmail.com>
2016-01-05 17:58:41 +0000

    Home view

--- Muvr/Entities/MRManagedClassifiedExercise+CoreDataAnalytics.swift
+++ Muvr/Entities/MRManagedClassifiedExercise+CoreDataAnalytics.swift
@@ -1,38 +1,133 @@
 import Foundation
+import MuvrKit
 import CoreData
 
 extension MRManagedClassifiedExercise {
     
-    static func summary(inManagedObjectContext managedObjectContext: NSManagedObjectContext) {
-        // volume: sum(duration)
-        // weight: sum(weight)
-        // intensity: sum(intensity)
-        // fitness: (weight * volume * intensity) / count
+    struct Average {
+        let exerciseId: MKExerciseId
+        let count: Int
+
+        let averageIntensity: Double
+        let averageRepetitions: Double
+        let averageWeight: Double
+        let averageDuration: NSTimeInterval
         
-        func sumFor(keyPath: String) -> NSExpressionDescription {
-            let expr = NSExpressionDescription()
-            expr.name = "\(keyPath)Sum"
-            expr.expression = NSExpression(forFunction: "sum:", arguments: [NSExpression(forKeyPath: keyPath)])
-            expr.expressionResultType = NSAttributeType.DoubleAttributeType
-            return expr
+        static func zero(exerciseId: MKExerciseId) -> Average {
+            return Average(exerciseId: exerciseId, count: 0, averageIntensity: 0, averageRepetitions: 0, averageWeight: 0, averageDuration: 0)
         }
         
+        private func plus(that: Average) -> Average {
+            assert(that.exerciseId == self.exerciseId)
+            
+            return Average(exerciseId: exerciseId,
+                count: count + that.count,
+                averageIntensity: averageIntensity + that.averageIntensity,
+                averageRepetitions: averageRepetitions + that.averageRepetitions,
+                averageWeight: averageWeight + that.averageWeight,
+                averageDuration: averageDuration + that.averageDuration
+            )
+        }
+        
+        private func div(by: Double) -> Average {
+            return Average(exerciseId: exerciseId,
+                count: count,
+                averageIntensity: averageIntensity / by,
+                averageRepetitions: averageRepetitions / by,
+                averageWeight: averageWeight / by,
+                averageDuration: averageDuration / by
+            )
+        }
+    }
+    
+    private static let countByEntity: NSExpressionDescription = {
         let count = NSExpressionDescription()
         count.name = "count"
-        count.expression = NSExpression(forFunction: "count:", arguments: [NSExpression(forKeyPath: "duration")])
+        count.expression = NSExpression(forFunction: "count:", arguments: [NSExpression(forKeyPath: "entity")])
         count.expressionResultType = NSAttributeType.Integer32AttributeType
-        
-        let entity = NSEntityDescription.entityForName("MRManagedClassifiedExercise", inManagedObjectContext: managedObjectContext)
-        let exerciseSession = entity!.propertiesByName["exerciseSession"]!
-        
-        let fetchRequest = NSFetchRequest(entityName: "MRManagedClassifiedExercise")
-        fetchRequest.propertiesToFetch = [count, sumFor("duration"), sumFor("cdWeight"), sumFor("cdIntensity"), sumFor("cdRepetitions")]
-        fetchRequest.propertiesToGroupBy = [exerciseSession]
-        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
-        
-        let result = try? managedObjectContext.executeFetchRequest(fetchRequest)
-        NSLog("\(result)")
+        return count
+    }()
+    
+    private static func averageFor(keyPath: String) -> NSExpressionDescription {
+        let expr = NSExpressionDescription()
+        expr.name = "\(keyPath)"
+        expr.expression = NSExpression(forFunction: "average:", arguments: [NSExpression(forKeyPath: keyPath)])
+        expr.expressionResultType = NSAttributeType.DoubleAttributeType
+        return expr
     }
+    
+    private static func componentAt(index: Int, exerciseId: String) -> String? {
+        let components = exerciseId.componentsSeparatedByString("/")
+        if index >= 0 && index < components.count { return components[index] }
+        return nil
+    }
+
+    static func averages(inManagedObjectContext managedObjectContext: NSManagedObjectContext, exerciseIdPrefix: String?) -> [Average] {
+        let entity = NSEntityDescription.entityForName("MRManagedClassifiedExercise", inManagedObjectContext: managedObjectContext)
+        let exerciseId = entity!.propertiesByName["exerciseId"]!
+
+        let fetchRequest = NSFetchRequest(entityName: "MRManagedClassifiedExercise")
+        fetchRequest.propertiesToFetch = [exerciseId, countByEntity, averageFor("duration"), averageFor("cdWeight"), averageFor("cdIntensity"), averageFor("cdRepetitions")]
+        fetchRequest.propertiesToGroupBy = [exerciseId]
+        if let exerciseIdPrefix = exerciseIdPrefix {
+            fetchRequest.predicate = NSPredicate(format: "exerciseId LIKE %@", exerciseIdPrefix + "*")
+        }
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "exerciseSession.start", ascending: false)]
+        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+        fetchRequest.fetchLimit = 100
+        
+        // exerciseIdComponentIndex is the number of "/" in ``exerciseIdPrefix`` if given or 0
+        let exerciseIdComponentIndex = exerciseIdPrefix.map { $0.characters.reduce(0) { r, c in if c == "/" { return r + 1 } else { return r } } } ?? 0
+        
+        if let result = (try? managedObjectContext.executeFetchRequest(fetchRequest)) as? [NSDictionary] {
+            var averages: [MKExerciseId : [Average]] = [:]
+            for entry in result {
+                if let exerciseId = componentAt(exerciseIdComponentIndex, exerciseId: entry["exerciseId"] as! String) {
+                    let average = Average(
+                        exerciseId: exerciseId,
+                        count: (entry["count"] as! NSNumber).integerValue,
+                        averageIntensity: (entry["cdIntensity"] as! NSNumber).doubleValue,
+                        averageRepetitions: (entry["cdRepetitions"] as! NSNumber).doubleValue,
+                        averageWeight: (entry["cdWeight"] as! NSNumber).doubleValue,
+                        averageDuration: (entry["duration"] as! NSNumber).doubleValue
+                    )
+                    
+                    if let existingAverages = averages[exerciseId] {
+                        averages[exerciseId] = existingAverages + [average]
+                    } else {
+                        averages[exerciseId] = [average]
+                    }
+                }
+            }
+            return averages.values.map { averages in
+                let z = Average.zero(averages.first!.exerciseId)
+                let reduced = averages.reduce(z) { $0.plus($1) }
+                return reduced.div(Double(averages.count))
+            }
+        }
+        
+        return []
+    }
+//    
+//    static func summary(inManagedObjectContext managedObjectContext: NSManagedObjectContext) {
+//        // volume: sum(duration)
+//        // weight: sum(weight)
+//        // intensity: sum(intensity)
+//        // fitness: (weight * volume * intensity) / count
+//        
+//        let entity = NSEntityDescription.entityForName("MRManagedClassifiedExercise", inManagedObjectContext: managedObjectContext)
+//        let exerciseSession = entity!.propertiesByName["exerciseSession"]!
+//        
+//        let fetchRequest = NSFetchRequest(entityName: "MRManagedClassifiedExercise")
+//        fetchRequest.propertiesToFetch = [countByEntity, sumFor("duration"), sumFor("cdWeight"), sumFor("cdIntensity"), sumFor("cdRepetitions")]
+//        fetchRequest.propertiesToGroupBy = [exerciseSession]
+//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "exerciseSession.start", ascending: false)]
+//        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+//        fetchRequest.fetchLimit = 100
+//        
+//        let result = try? managedObjectContext.executeFetchRequest(fetchRequest)
+//        NSLog("\(result)")
+//    }
     
 
 }
--- Muvr/MRHomeViewController.swift
+++ Muvr/MRHomeViewController.swift
@@ -1,9 +1,86 @@
 import UIKit
+import Charts
 
-class MRHomeViewController : UIViewController {
+class MRHomeViewController : UIViewController, ChartViewDelegate {
 
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var pieChartBackButton: UIButton!
+
+    private var averages: [MRManagedClassifiedExercise.Average] = []
+
+    private var exerciseIdPrefix: String?
+    private var transform: MRManagedClassifiedExercise.Average -> Double = { Double($0.count) }
+
+    override func viewDidLoad() {
+        pieChartBackButton.hidden = true
+        
+        pieChartView.delegate = self
+        pieChartView.usePercentValuesEnabled = true
+        pieChartView.holeTransparent = true
+        pieChartView.holeRadiusPercent = 0.58
+        pieChartView.transparentCircleRadiusPercent = 0.61
+        pieChartView.descriptionText = ""
+
+        pieChartView.drawHoleEnabled = true
+        pieChartView.rotationAngle = 0.0
+        pieChartView.rotationEnabled = true
+        pieChartView.highlightPerTapEnabled = true
+        
+        pieChartView.usePercentValuesEnabled = false
+        
+        pieChartView.legend.enabled = false
+    }
+    
     override func viewDidAppear(animated: Bool) {
-        MRManagedClassifiedExercise.summary(inManagedObjectContext: MRAppDelegate.sharedDelegate().managedObjectContext)
+        averages = MRManagedClassifiedExercise.averages(inManagedObjectContext: MRAppDelegate.sharedDelegate().managedObjectContext, exerciseIdPrefix: exerciseIdPrefix)
+        reloadAveragesChart()
+    }
+    
+    private func reloadAveragesChart() {
+        var ys: [BarChartDataEntry] = []
+        var xs: [String] = []
+        for (index, average) in averages.enumerate() {
+            ys.append(BarChartDataEntry(value: transform(average), xIndex: index, data: average.exerciseId))
+            xs.append(average.exerciseId)
+        }
+        let dataSet = PieChartDataSet(yVals: ys)
+        dataSet.colors = ChartColorTemplates.pastel() + ChartColorTemplates.liberty()
+        if ys.count > 1 { dataSet.sliceSpace = 2 }
+        
+        pieChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0, easingOption: ChartEasingOption.EaseOutCirc)
+        pieChartView.data = PieChartData(xVals: xs, dataSet: dataSet)
+        pieChartView.highlightValues([])
+    }
+    
+    @IBAction
+    func resetExerciseIdPrefix() {
+        exerciseIdPrefix = nil
+        averages = MRManagedClassifiedExercise.averages(inManagedObjectContext: MRAppDelegate.sharedDelegate().managedObjectContext, exerciseIdPrefix: exerciseIdPrefix)
+        pieChartBackButton.hidden = true
+        reloadAveragesChart()
+    }
+    
+    @IBAction
+    func transformSelected(sender: UISegmentedControl) {
+        // # W R I D
+        switch sender.selectedSegmentIndex {
+        case 0: /* # */ transform = { Double($0.count) }
+        case 1: /* W */ transform = { $0.averageWeight }
+        case 2: /* R */ transform = { $0.averageRepetitions }
+        case 3: /* I */ transform = { $0.averageIntensity }
+        case 4: /* D */ transform = { $0.averageDuration }
+        default: fatalError("Match error")
+        }
+        reloadAveragesChart()
+    }
+    
+    func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
+        if let exerciseId = entry.data as? String where exerciseIdPrefix == nil {
+            exerciseIdPrefix = exerciseId + "/"
+            pieChartBackButton.hidden = false
+            averages = MRManagedClassifiedExercise.averages(inManagedObjectContext: MRAppDelegate.sharedDelegate().managedObjectContext, exerciseIdPrefix: exerciseIdPrefix)
+            reloadAveragesChart()
+        }
     }
     
 }
