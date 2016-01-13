6dc2e1838450d96758b1aaa957ac07d2d8698899
Jan Machacek <jan.machacek@gmail.com>
2016-01-13 17:39:46 +0000

    x

--- MuvrKit/MKPolynomialFittingPredictor.swift
+++ MuvrKit/MKPolynomialFittingPredictor.swift
@@ -4,11 +4,15 @@ public class MKPolynomialFittingPredictor<A where A : Hashable> : MKPredictor {
     public typealias K = A
     private var coefficients: [K:[Float]] = [:]
     
-    public func train(x x: [Double], y: [Double], forKey key: K) {
-        
+    public func trainPositional(x x: [Int], y: [Double], forKey key: K) throws {
+        let c = try MKPolynomialFitter.fit(x: x.map { Float($0) }, y: y.map { Float($0) }, degree: 5)
+        coefficients[key] = c
     }
     
     public func predicAt(x: Int, forKey key: K) -> Double? {
+        if let coefficients = coefficients[key] {
+            
+        }
         return 0
     }
     
