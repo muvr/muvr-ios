8915257eb538404fe6e1c58dd1209e1333752001
Jan Machacek <jan.machacek@gmail.com>
2016-01-18 09:51:44 +0000

    Comments I

--- Muvr/MRAppDelegate+UITesting.swift
+++ Muvr/MRAppDelegate+UITesting.swift
@@ -12,57 +12,8 @@ import MuvrKit
 //
 extension MRAppDelegate  {
     
-//    ///
-//    /// Comments
-//    ///
-//    private func exerciseIds() -> [String] {
-//        return [
-//            "resistanceTargeted:arms/biceps-curl", "resistanceTargeted:arms/triceps-extension", "resistanceTargeted:shoulders/lateral-raise", "resistanceTargeted:arms/reverse-cable-curl",
-//            "resistanceTargeted:legs/squat", "resistanceTargeted:legs/lunges", "resistanceTargeted:legs/dumbbell-calf-raise",
-//            "resistanceTargeted:back/barbell-row", "resistanceTargeted:back/back-extensions", "resistanceTargeted:back/lateral-pulldown-straight",
-//            "resistanceTargeted:core/oblique-crunches", "resistanceTargeted:core/suitcase-crunches", "resistanceTargeted:core/side-dips", "resistanceTargeted:core/crunches",
-//            "resistanceTargeted:chest/dumbbell-flyes", "resistanceTargeted:chest/angle-chest-press", "resistanceTargeted:chest/butterfly-machine"
-//        ]
-//    }
-//    
-    private func generateSessionData(date date: NSDate) {
-        
-        func generateClassifiedExercise(date date: NSDate, session: MRManagedExerciseSession, index: Int) {
-//            let exercise = MRManagedClassifiedExercise.insertNewObject(inManagedObjectContext: managedObjectContext)
-//            exercise.confidence = 1
-//            exercise.exerciseId = exerciseIds()[index % exerciseIds().count]
-//            exercise.exerciseSession = session
-//            exercise.duration = 20 + NSTimeInterval(arc4random() % 30)
-//            exercise.cdIntensity = 1
-//            exercise.cdRepetitions = 5 + Int(arc4random() % 10)
-//            exercise.cdWeight = Double(arc4random() % 50)
-//            exercise.start = date.addSeconds(index * 60)
-        }
-
-//        let session = MRManagedExerciseSession.insertNewObject(inManagedObjectContext: managedObjectContext)
-//        session.id = NSUUID().UUIDString
-//        session.exerciseModelId = "arms"
-//        session.start = date
-//        session.completed = true
-//        session.uploaded = true
-        
-//        (0..<30).forEach { i in generateClassifiedExercise(date: date, session: session, index: i) }
-        // (0..<2).forEach { i in generateLabelledExercise(date: date, session: session, index: i) }
-    }
-    
-    private func getSessionDates() -> [NSDate] {
-        let today = NSDate()
-        let earlierToday = today.addHours(-2)
-        var dates = [today, earlierToday]
-        (1..<10).forEach { i in
-            dates.appendContentsOf([today.addDays(-i), earlierToday.addDays(-i)])
-        }
-        return dates
-    }
-    
     private func generateData() {
-        getSessionDates().forEach(generateSessionData)
-        saveContext()
+        fatalError("Implementation is missing")
     }
 
     func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
