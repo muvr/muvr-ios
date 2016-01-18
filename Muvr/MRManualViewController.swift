6d79096d8ef9e8b7d07aa3153fe0a4e183254abc
Jan Machacek <jan.machacek@gmail.com>
2016-01-18 10:16:25 +0000

    nil cells

--- Muvr/MRManualViewController.swift
+++ Muvr/MRManualViewController.swift
@@ -13,10 +13,55 @@ class MRManualViewController : UIViewController, UITableViewDataSource, UITableV
         .Arms, .Back, .Chest, .Core, .Shoulders, .Legs
     ]
     
+    /// A list of our checked things. Use ``keyFromIndexPath`` to compute the key Int.
+    /// The value indicates whether the item is checked or not.
+    private var checked: [Int:Bool] = [:]
+    
     @IBAction private func start() {
         if let exerciseType = exerciseType {
             try! MRAppDelegate.sharedDelegate().startSessionForExerciseType(exerciseType, start: NSDate(), id: NSUUID().UUIDString)
         }
+    }
+    
+    /// Computes the key from the given ``indexPath``
+    /// - parameter indexPath: the IP
+    /// - returns: the key to be used in the ``checked`` dictionary
+    private func keyFromIndexPath(indexPath: NSIndexPath) -> Int {
+        return indexPath.section * 1024 + indexPath.row
+    }
+    
+    /// Indicates whether the value is checked at the given ``indexPath``
+    /// - parameter indexPath: the IP
+    /// - returns: checked or not
+    private func isCheckedAtIndexPath(indexPath: NSIndexPath) -> Bool {
+        return checked[keyFromIndexPath(indexPath)] ?? false
+    }
+    
+    /// Sets the checked value at the given ``indexPath``
+    /// - parameter indexPath: the IP
+    /// - parameter value: the new value
+    private func setCheckedAtIndexPath(indexPath: NSIndexPath, value: Bool) {
+        checked[keyFromIndexPath(indexPath)] = value
+    }
+    
+    /// Returns the cell accessory type for the given ``indexPath``
+    /// - parameter indexPath: the IP
+    /// - returns: the accessory type
+    private func accessoryTypeForIndexPath(indexPath: NSIndexPath) -> UITableViewCellAccessoryType {
+        if isCheckedAtIndexPath(indexPath) {
+            return .Checkmark
+        } else {
+            return .None
+        }
+    }
+    
+    /// Inverts the checked value at the ``indexPath``
+    /// - parameter indexPath: the IP
+    /// - returns: the new value
+    private func toggleCheckedAtIndexPath(indexPath: NSIndexPath) -> Bool {
+        let value = !isCheckedAtIndexPath(indexPath)
+        setCheckedAtIndexPath(indexPath, value: value)
+        return value
     }
 
     // MARK: - UITableViewDataSource
@@ -41,10 +86,12 @@ class MRManualViewController : UIViewController, UITableViewDataSource, UITableV
         case 0:
             let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
             cell.textLabel?.text = muscleGroups[indexPath.row].title
+            cell.accessoryType = accessoryTypeForIndexPath(indexPath)
             return cell
         case 1, 2:
             let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
             cell.textLabel?.text = exerciseTypeDescriptors[indexPath.section].title
+            cell.accessoryType = accessoryTypeForIndexPath(indexPath)
             return cell
         default: fatalError()
         }
@@ -54,22 +101,20 @@ class MRManualViewController : UIViewController, UITableViewDataSource, UITableV
         // first, deselect all other cells in all other sections
         for s in 0..<numberOfSectionsInTableView(tableView) where s != indexPath.section {
             for r in 0..<self.tableView(tableView, numberOfRowsInSection: s) {
-                tableView.cellForRowAtIndexPath(NSIndexPath(forRow: r, inSection: s))!.accessoryType = .None
+                let ip = NSIndexPath(forRow: r, inSection: s)
+                setCheckedAtIndexPath(ip, value: false)
+                tableView.cellForRowAtIndexPath(ip)?.accessoryType = .None
             }
         }
 
-        let cell = tableView.cellForRowAtIndexPath(indexPath)!
-        if cell.accessoryType == .None {
-            cell.accessoryType = .Checkmark
-        } else {
-            cell.accessoryType = .None
-        }
+        let checked = toggleCheckedAtIndexPath(indexPath)
+        tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = accessoryTypeForIndexPath(indexPath)
         
         switch indexPath.section {
         case 0:
             var selectedMuscleGroups: [MKMuscleGroup] = []
             for r in 0..<self.tableView(tableView, numberOfRowsInSection: indexPath.section) {
-                if tableView.cellForRowAtIndexPath(NSIndexPath(forRow: r, inSection: indexPath.section))!.accessoryType == .Checkmark {
+                if isCheckedAtIndexPath(NSIndexPath(forRow: r, inSection: indexPath.section)) {
                     selectedMuscleGroups.append(muscleGroups[r])
                 }
             }
@@ -78,9 +123,9 @@ class MRManualViewController : UIViewController, UITableViewDataSource, UITableV
             } else {
                 exerciseType = .ResistanceTargeted(muscleGroups: selectedMuscleGroups)
             }
-        case 1 where cell.accessoryType == .Checkmark:
+        case 1 where checked:
             exerciseType = .ResistanceWholeBody
-        case 2 where cell.accessoryType == .Checkmark:
+        case 2 where checked:
             exerciseType = .IndoorsCardio
         default:
             exerciseType = nil
