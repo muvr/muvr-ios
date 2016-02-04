a6b95c7acb268800f6b7065b3e627a07bb6239c4
Damien Bailly <damien.bailly@gmail.com>
2016-02-04 14:39:19 +0000

    Remove constant columns in linear regression

--- MuvrKit/MKLinearRegression.swift
+++ MuvrKit/MKLinearRegression.swift
@@ -47,31 +47,103 @@ struct MKLinearRegression {
     /// returns an array of length (1 + m * degree) containing the θ coefficients
     ///
     static func train(x: [Float], y: [Float], m: Int, degree: Int) -> [Float] {
-        let k = degree * m + 1
         let n = x.count / m
-        let x_ = format(x, m: m, degree: degree)
+        let k = degree * m + 1
         
-        let onlyOneSample = x.enumerate().reduce(true) { same, entry in
-            let (i, v) = entry
+        var dims = [Bool](count: m, repeatedValue: false)
+        dims = x.enumerate().reduce(dims) { (var dims, e) in
+            let (i, v) = e
             let col = i % m
-            if v != x[col] { return false }
-            return same
+            if v != x[col] {
+                dims[col] = true
+            }
+            return dims
         }
         
-        var θ = [Float](count: k, repeatedValue: 0)
+        let m_ = dims.filter { $0 }.count
+        let k_ = degree * m_ + 1
+        let x_ = validInput(x, dims: dims, degree: degree) // remove constant columns from x
         
-        if onlyOneSample {
+        if x_.isEmpty {
+            var θ = [Float](count: k, repeatedValue: 0)
             θ[0] = y[0]
             return θ
         }
         
-        let X = la_matrix_from_float_buffer(x_, la_count_t(n), la_count_t(k), la_count_t(k), la_hint_t(LA_NO_HINT), la_attribute_t(LA_DEFAULT_ATTRIBUTES))
-        let Y = la_matrix_from_float_buffer(y, la_count_t(n), la_count_t(1), la_count_t(1), la_hint_t(LA_NO_HINT), la_attribute_t(LA_DEFAULT_ATTRIBUTES))
-    
-        /// Solves Θ in the equation (X'.X).Θ = (X'.Y)
-        let Θ = la_solve(la_matrix_product(la_transpose(X), X), la_matrix_product(la_transpose(X), Y))
+        let defaultAttr = la_attribute_t(LA_DEFAULT_ATTRIBUTES)
+        let noHint = la_hint_t(LA_NO_HINT)
         
-        la_vector_to_float_buffer(&θ, 1, Θ)
+        let input = format(x_, m: m_, degree: degree) // add columns for all degrees
+        
+        let X = la_matrix_from_float_buffer(input, la_count_t(n), la_count_t(k_), la_count_t(k_), noHint, defaultAttr)
+        let Y = la_matrix_from_float_buffer(y, la_count_t(n), la_count_t(1), la_count_t(1), noHint, defaultAttr)
+        let X_ = la_transpose(X)
+        
+        /// Solves Θ in the equation (X'.X).Θ = (X'.Y)
+        let Θ = la_solve(la_matrix_product(X_, X), la_matrix_product(X_, Y))
+        
+        let θ = fixCoefficients(Θ, dims: dims, degree: degree)
+        
+        if θ.isEmpty {
+            // Couldn't solve, return a constant value
+            var θ = [Float](count: k, repeatedValue: 0)
+            θ[0] = y[0]
+            return θ
+        }
+        
+        return θ
+    }
+    
+    ///
+    /// Remove columns containing always the same value
+    /// - parameter x: the original input matrix (size n x m)
+    /// - parameter dims: array of size m where true indicate valid columns, invalid columns are removed from x
+    /// - parameter degree: the degree used in the linear regression
+    /// - return a matrix of size (n x m_) where m_ is the number of valid dimensions
+    private static func validInput(x: [Float], dims: [Bool], degree: Int) -> [Float] {
+        let m = dims.count
+        let n = x.count / m
+        
+        let m_ = dims.filter { $0 }.count
+        
+        var x_ = [Float](count: m_ * n, repeatedValue: 0)
+        var j = 0
+        for i in 0..<(m * n) {
+            let col = i % m
+            if dims[col] { x_[j++] = x[i] }
+        }
+        return x_
+    }
+    
+    ///
+    /// Θ has size (1 x k_): contains only coefficients for valid columns
+    /// but need to return an array of size k where invalid columns have 0 coefficients
+    /// - parameter Θ: the coefficient matrix returned by la_solve
+    /// - parameter dims: array of size m where true indicate valid columns
+    /// - parameter degree: the degree used in the linear regression
+    /// - return an array of length k (or empty if Θ is empty)
+    ///
+    private static func fixCoefficients(Θ: la_object_t, dims: [Bool], degree: Int) -> [Float] {
+        guard la_matrix_cols(Θ) > 0 else { return [] }
+        
+        let m = dims.count
+        let k = degree * m + 1
+        
+        let m_ = dims.filter { $0 }.count
+        let k_ = degree * m_ + 1
+        
+        var θ_ = [Float](count: k_, repeatedValue: 0)
+        la_vector_to_float_buffer(&θ_, 1, Θ)
+        
+        var θ = [Float](count: k, repeatedValue: 0)
+        var j = 0
+        for i in 0..<k {
+            if i == 0 { θ[0] = θ_[j++] }
+            else {
+                let col = (i - 1) % m
+                if dims[col] { θ[i] = θ_[j++] }
+            }
+        }
         
         return θ
     }
--- MuvrKitTests/MKLinearRegressionTests.swift
+++ MuvrKitTests/MKLinearRegressionTests.swift
@@ -32,4 +32,24 @@ class MKRegressionFitterTests : XCTestCase {
         }
     }
     
+    func testTrainXDropDim() {
+        let d = 2
+        let m = 2
+        
+        let x: [Float] = [1,20, 1,25, 1,25, 1,30] // 4x2
+        let y: [Float] = [4, 6, 6, 8] // 1x4
+        let θ = MKLinearRegression.train(x, y: y, m: m, degree: d)
+        
+        print(θ)
+        XCTAssertEqual(0, θ[1])
+        XCTAssertEqual(0, θ[3])
+        
+        let n = x.count / m
+        for i in 0..<n {
+            let row = Array(x[(i*m)..<(i+1)*m])
+            let res = MKLinearRegression.estimate(row, θ: θ)
+            XCTAssertEqualWithAccuracy(y[i], res, accuracy: 0.05)
+        }
+    }
+    
 }
