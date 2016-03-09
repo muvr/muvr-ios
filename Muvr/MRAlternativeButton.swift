e2ae1a744f18fca9f8483af29925ec862bd353ff
Jan Machacek <jan.machacek@gmail.com>
2016-03-09 09:23:13 +0000

    Tweaks

--- Muvr/MRAlternativeButton.swift
+++ Muvr/MRAlternativeButton.swift
@@ -10,17 +10,25 @@ class MRAlternativeButton: UIButton {
         }
     }
     
+    @IBInspectable
+    var lineWidth: NSNumber? {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
         let radius = min(frame.width, frame.height) / 2
-        let lineWidth = radius / 16
+        let radius16 = radius / 16
+        let lineWidth = CGFloat(self.lineWidth?.floatValue ?? radius16)
         
         titleLabel?.numberOfLines = 3
         titleLabel?.lineBreakMode = .ByWordWrapping
         titleLabel?.textAlignment = .Center
         titleLabel?.adjustsFontSizeToFitWidth = true
-        titleLabel?.font = titleLabel?.font.fontWithSize(4.2 * lineWidth)
+        titleLabel?.font = titleLabel?.font.fontWithSize(4.2 * radius16)
         
         titleEdgeInsets = UIEdgeInsets(top: 4, left: 8, bottom: 4, right: 8)
         
