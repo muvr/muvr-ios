63deeeb5b027e563a4526e94deea6caf5dd43f28
Damien Bailly <damien.bailly@gmail.com>
2016-04-06 10:57:27 +0100

    Pebble Watch data decoding

--- MuvrKit/MKSensorData+Decoding.swift
+++ MuvrKit/MKSensorData+Decoding.swift
@@ -58,14 +58,55 @@ public extension MKSensorData {
 
         var samples: [Float] = [Float](count: Int(valuesCount), repeatedValue: 0)
         
-        for i in 0..<Int(valuesCount) {
-            switch(device!) {
-            case .AppleWatch: samples[i] = try bytes.next()
-            case .Pebble: throw MKCodecError.NotSupported
+        switch(device!) {
+        case .AppleWatch:
+            for i in 0..<Int(valuesCount) { samples[i] = try bytes.next() }
+        case .Pebble:
+            for i in 0..<Int(valuesCount) / 3 {
+                let (x, y, z) = try bytes.nextPebbleSample()
+                samples[3 * i] = x
+                samples[3 * i + 1] = y
+                samples[3 * i + 2] = z
             }
-            
         }
+        
         
         try self.init(types: types, start: start, samplesPerSecond: UInt(samplesPerSecond), samples: samples)
     }
 }
+
+private extension MKUnsafeBufferReader {
+
+    /// read n bytes from the bytes buffer
+    /// and return them into an array of UInt8
+    func nextBytes(n: Int) throws -> [UInt8] {
+        var bytes = [UInt8](count: n, repeatedValue: 0)
+        for i in 0..<bytes.count {
+            bytes[i] = try next()
+        }
+        return bytes
+    }
+    
+    /// Reads 5 bytes from the given buffer corresponding to x,y,z values.
+    /// x,y,z values are encoded on 13, 13 and 14 bits respectively (total: 40 bits)
+    /// - returns the x,y,z values as a tuple
+    func nextPebbleSample() throws -> (Float, Float, Float) {
+        // read 40 bits and store each of them into UInt16 to ease bit handling
+        var ints = try nextBytes(5).map { UInt16($0) }
+        
+        // Move bits around to get the correct values
+        var z = (ints[4] << 6) | (ints[3] >> 2) // 14 bits
+        var y = ((ints[3] & 0x03) << 10) | (ints[2] << 3) | (ints[1] >> 5) // 13 bits
+        var x = ((ints[1] & 0x1F) << 8) | ints[0] // 13 bits
+        
+        // Handle signs
+        if z & 0x2000 > 0 { z |= 0xC000 }
+        if y & 0x1000 > 0 { y |= 0xE000 }
+        if x & 0x1000 > 0 { x |= 0xE000 }
+        
+        // Convert to signed float
+        return (Float(Int16(bitPattern: x)), Float(Int16(bitPattern: y)), Float(Int16(bitPattern: z)))
+    }
+    
+}
+
