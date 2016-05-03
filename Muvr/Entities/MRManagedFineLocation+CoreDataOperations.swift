9302f983b171b3ec615b4aab7fe76711762d9a9a
Jan Machacek <jan.machacek@gmail.com>
2016-05-03 21:00:55 +0100

    Exercises

--- Muvr/Entities/MRManagedFineLocation+CoreDataOperations.swift
+++ Muvr/Entities/MRManagedFineLocation+CoreDataOperations.swift
@@ -5,8 +5,45 @@ extension MRManagedFineLocation {
     
     static func exerciseIdsAtMajor(major: NSNumber, minor: NSNumber) -> [MKExercise.Id] {
         if minor.integerValue == 1 {
-            return ["resistanceTargeted:arms/dumbbell-biceps-curl"]
+            return [
+                "resistanceTargeted:arms/dumbbell-biceps-curl",
+                "resistanceTargeted:chest/dumbbell-flyes",
+                "resistanceTargeted:legs/dumbbell-calf-raise",
+                "resistanceTargeted:shoulders/dumbbell-shoulder-press",
+                "resistanceTargeted:shoulders/dumbbell-bench-press",
+            ]
         }
+        if minor.integerValue == 2 {
+            return [
+                "resistanceTargeted:arms/cable-triceps-pushdown",
+                "resistanceTargeted:arms/cable-hammer-curl",
+                "resistanceTargeted:arms/cable-biceps-curl",
+                "resistanceTargeted:arms/reverse-cable-curl",
+                "resistanceTargeted:arms/overhead-cable-curl",
+                "resistanceTargeted:chest/cable-crossover",
+                "resistanceTargeted:back/cable-row",
+                "resistanceTargeted:core/cable-wood-chop",
+                "resistanceTargeted:shoulders/cable-standing-row"
+            ]
+        }
+        if minor.integerValue == 3 {
+            return [
+                "resistanceTargeted:core/russian-twist",
+                "resistanceTargeted:core/dragonfly",
+                "resistanceTargeted:core/crunches"
+            ]
+        }
+        if minor.integerValue == 4 {
+            return [
+                "resistanceTargeted:legs/barbell-squat",
+                "resistanceTargeted:legs/squat",
+                "resistanceTargeted:legs/sumo-squat",
+                "resistanceTargeted:legs/leg-press",
+                "resistanceTargeted:legs/barbell-deadlift",
+                "resistanceTargeted:legs/lunges"
+            ]
+        }
+        
         return []
     }
     
